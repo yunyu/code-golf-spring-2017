6b625428e723a52893f1b3c58da17e5ba93dc5cd
Yunyu Lin <yunyu.lin@vanderbilt.edu>
2017-02-18 12:55:16 -0600

    Add problem 1

---
+++ solutions/prob1.py
@@ -0,0 +1,12 @@
+import sys
+def npb(n):
+	c=0
+	np=[]
+	for i in range(2,n+1):
+		if i not in np:
+			c=c+1
+			for j in range(i*i,n+1,i):
+				np.append(j)
+	return c
+for l in tuple(open('../inputs/1.txt', 'r')):
+	print>>open('../answers/1.txt', 'a'),npb(int(l))
