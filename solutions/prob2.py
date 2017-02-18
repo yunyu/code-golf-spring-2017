6c77ee1720f615d3fd1d569c00e8af0b6b3f2532
Yunyu Lin <yunyu.lin@vanderbilt.edu>
2017-02-18 14:25:49 -0600

    Problem 2

--- solutions/prob2.py
+++ solutions/prob2.py
@@ -1,35 +1,16 @@
-def run(a,b,o):
-    print a,o,b
-    z = 0
-    if o == '+':
-        z = a + b
-    if o == '-':
-        z = a - b
-    if o == '*':
-        z = a * b
-    if o == '/':
-        z = a / b
-    return z
+from collections import deque
+def p(t):
+    t0=t.popleft()
+    if t0=='+':
+            return p(t)+p(t)
+    elif t0=='-':
+            return p(t)-p(t)
+    elif t0=='*':
+            return p(t)*p(t)
+    elif t0=='/':
+            return p(t)/p(t)
+    else:
+            return int(t0)
 
-for n in tuple(open('../inputs/2.txt', 'r')):
-    v = 0
-    stack = []
-    lst=[]
-    for i, c in enumerate(n):
-        if not c.isdigit():
-            stack.append(c)
-        else:
-            c = ord(c) - 48
-            if i+2 < len(n):
-                if n[i+1].isdigit():
-                    o = ord(n[i+1]) - 48
-                    op = stack.pop()
-                    v += run(o,c,op)
-                else:
-                    lst.append(c)
-    stack.pop()
-    print stack,lst
-    while len(stack) > 0:
-        v = run(lst.pop(), v, stack.pop())
-    print v
-
+for l in open('../inputs/2.txt', 'r'):
+    print>>open('../answers/2.txt', 'a'),p(deque(list(l.strip())))
